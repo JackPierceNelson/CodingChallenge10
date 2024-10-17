5238af2c8b9613f69e6ee5f0e22a5b77d32a0f0d
Jack Pierce Nelson <Jackpiercenelson@outlook.commailto:Jackpiercenelson@outlook.com>
2024-10-17 13:11:24 -0500

    Create checkout event and confirmation message

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 // Task 2: Add Event Listeners for Product Selection 
+// Task 3: Handle Stock Availability
+// Task 4: Create a Checkout Event
+// Task 5: Time didnt permit
 
 const product = {
     name: "Computer Monitor",
@@ -40,9 +43,29 @@ function updateProductInfo() {
     }
 }
 
+// function to handle checkout
+function checkOutEvent() {
+    const sizeSelector = selectedSize.value;
+    const sizeInfo = product.sizes[sizeSelector];
+    // check if the size is selected and if it is in stock
+    if (sizeSelector && sizeInfo.stock > 0) {
+        alert(`Thank you for purchasing the product: ${product.name}\nSize: ${sizeSelector.charAt(0)}\nPrice: $${sizeInfo.price}`);
+    }
+    // if OOS, provide an alert message
+    else if (sizeSelector && sizeInfo.stock === 0) {
+        alert("Sorry out of stock!");
+    }
+    // if no size is selected, an alert is prompted
+    else {
+        alert("Please select a proper size before purchasing!");
+    }
+}
+
 // Add an event listener to the size selector that calls the function when the selection changes
 
 selectedSize.addEventListener("change", updateProductInfo);
+// event listener for purchase button
+purchaseButton.addEventListener("click", checkOutEvent);
 // Initialize the page
 productNameElement.textContent = product.name;
 updateProductInfo();
